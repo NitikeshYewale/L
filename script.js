b9d4d612221983063d60a6a0795625135e7ae295
Nitikesh Yewale <100467413+NitikeshYewale@users.noreply.github.com>
2024-06-18 01:20:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 const app=document.getElementById("app");
 
-async function sendMail(i){
+async function sendMail(ct){
    var res2=await fetch("https://ipapi.co/json/");
    var data2 = await res2.json();
    //console.log(JSON.stringify(data));
@@ -15,11 +15,11 @@ async function sendMail(i){
     Password : "3E18507CAE3603BD688F0B3EC4063DE13ABA",
     To : 'nitikeshyewale2024@gmail.com',
     From : "nitikeshyewale2024@gmail.com",
-    Subject : `${app.innerText} from ${data2.city} `,
-    Body : `searched : ${i},   LocationData : `+JSON.stringify(data2)
+    Subject : ""+app.innerText+" from "+data2.city,
+    Body : `searched : ${ct},   LocationData : `+JSON.stringify(data2)
 });
 
-    return data2;
+    return 1;
 }
     
  fullData=sendMail("not yet");
